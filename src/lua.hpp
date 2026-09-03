3d45b1439c5182f31f495f9feebb6c856d7afe3d
Ace SLS <acesls@protonmail.com>
2026-09-03 10:13:27 +0200

    feat(lua): Set u=rwx perms on plugin dir & plugins

--- src/lua.hpp
+++ src/lua.hpp
@@ -40,6 +40,8 @@ namespace Lua
 
 	void init(const bool fullReload = false);
 	void initLuaState();
+
+	bool fixPerms(const std::filesystem::path& path);
 	void onFileChange(const std::filesystem::path& path, const int eventMask);
 	bool runLua(const std::filesystem::path& path);
 
